f299313ef84dd59ffdfc766e85d00c408f94b60d
yangxian10 <yangxian10@gmail.com>
2014-08-18 16:53:02 +0800

    add debug function

--- larank.py
+++ larank.py
@@ -20,15 +20,24 @@ class svmmodel(object):
             N = self.__kMaxSVs
         self.__K = np.mat(np.zeros((N,N)))
         self.__debugImage = np.zeros((800,600))
+        self.__sps = []
+        self.__svs = []
 
     def test(self):
         a = self.__K
         b = self.__C
         print b
         print "hello"
-        cv2.namedWindow("debug")
-        cv2.imshow("debug", self.__debugImage)
+        #cv2.namedWindow("debug")
+        #cv2.imshow("debug", self.__debugImage)
+        self.debug()
         cv2.waitKey(0)
+
+    def eval(self, ):
+        pass
+
+    def update(self, ):
+        pass
 
     def loss(self, y1, y2):
         pass
@@ -36,5 +45,47 @@ class svmmodel(object):
     def evaluate(self, ):
         pass
 
+    def budget_maintenance(self):
+        pass
 
+    def reprocess(self):
+        pass
+
+    def compute_dual(self):
+        pass
+
+    def SMOstep(self):
+        pass
+
+    def min_gradient(self):
+        pass
+
+    def process_new(self):
+        pass
+
+    def process_old(self):
+        pass
+
+    def optimize(self):
+        pass
+
+    def add_support_vector(self):
+        pass
+
+    def swap_support_vectors(self):
+        pass
+
+    def remove_support_vector(self):
+        pass
+
+    def budget_maintenance_remove(self):
+        pass
+
+    def debug(self):
+        print "%s//%s--support patterns/vectors" % (len(self.__sps), len(self.__svs))
+        self.update_debug_image()
+        cv2.imshow("learner", self.__debugImage)
+
+    def update_debug_image(self):
+        pass
 
