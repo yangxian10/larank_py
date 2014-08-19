dcb7bdb83c563f33415b1e5a2a0a1ebec0fa91d6
yangxian10 <yangxian10@gmail.com>
2014-08-19 16:35:40 +0800

    finish larank function

--- larank.py
+++ larank.py
@@ -1,19 +1,32 @@
 __author__ = 'yangxian'
 
 import numpy as np
+import sys
 import cv2
+import random
 
-class support_pattern(object):
-    pass
+class Support_pattern(object):
+    def __init__(self):
+        self.x = []
+        self.yv = []
+        self.images = []
+        self.y = 0
+        self.refCount = 1
 
-class support_vector(object):
-    pass
+class Support_vector(object):
+    def __init__(self):
+        self.x = []
+        self.y = 0
+        self.b = 0.0
+        self.g = 0.0
+        self.image = []
 
-class svmmodel(object):
+class SVM_model(object):
     def __init__(self, svmC=100.0, svmBudgetSize=100):
         self.__kTileSize = 30
         self.__kMaxSVs = 2000
         self.__C = svmC
+        self.__svmBudgetSize = svmBudgetSize
         if svmBudgetSize > 0:
             N = svmBudgetSize + 2
         else:
@@ -33,8 +46,9 @@ class svmmodel(object):
         self.debug()
         cv2.waitKey(0)
 
-    def eval(self, ):
-        pass
+    def eval(self, sample):
+
+        results = []
 
     def update(self, ):
         pass
@@ -42,44 +56,196 @@ class svmmodel(object):
     def loss(self, y1, y2):
         pass
 
-    def evaluate(self, ):
-        pass
+    def evaluate(self, x, yRect):
+        f = 0.0
+        for i in range(len(self.__svs)):
+            f += self.__svs[i].b * x.dot(self.__svs[i].x[self.__svs[i].y])
+        return f
 
     def budget_maintenance(self):
-        pass
+        if self.__svmBudgetSize > 0:
+            while len(self.__svs) > self.__svmBudgetSize:
+                self.budget_maintenance_remove()
 
     def reprocess(self):
-        pass
+        self.process_old()
+        for i in range(10):
+            self.optimize()
 
     def compute_dual(self):
-        pass
+        d = 0.0
+        for i in range(len(self.__svs)):
+            sv = self.__svs[i]
+            d -= sv.b * self.loss(sv.x.yv[sv.y], sv.x.yv[sv.x.y])
+            for j in range(len(self.__svs)):
+                d -= 0.5 * sv.b * self.__svs[j].b * self.__K[i,j]
+        return d
 
-    def SMOstep(self):
-        pass
+    def SMOstep(self, ipos, ineg):
+        if ipos == ineg:
+            return
+        svp = self.__svs[ipos]
+        svn = self.__svs[ineg]
+        assert svp.x == svn.x
+        sp = svp.x
 
-    def min_gradient(self):
-        pass
+        if svp.g - svn.g > 1e-5:
+            kii = self.__K[ipos,ipos] + self.__K[ineg,ineg] - self.__K[ipos,ineg]
+            ru = (svp.g - svn.g) / kii
+            r = min(ru, self.__C*int(svp.y == sp.y)-svp.b)
+            svp.b += r
+            svn.b -= r
+            for i in range(len(self.__svs)):
+                svi = self.__svs[i]
+                svi.g -= r * (self.__K[i,ipos] - self.__K[i,ineg])
 
-    def process_new(self):
-        pass
+        if abs(svp.b) < 1e-8:
+            self.remove_support_vector(ipos)
+            if ineg == len(self.__svs):
+                ineg = ipos
+
+        if abs(svn.b) < 1e-8:
+            self.remove_support_vector(ineg)
+
+    def min_gradient(self, index):
+        sp = self.__sps[index]
+        min_grad_index = -1
+        min_grad_val = sys.float_info.max
+        for i in range(len(sp.yv)):
+            grad = -self.loss(sp.yv[i], sp.yv[sp.y]) - self.evaluate(sp.x[i], sp.yv[i])
+            if grad < min_grad_val:
+                min_grad_index = i
+                min_grad_val = grad
+        return min_grad_index, min_grad_val
+
+    def process_new(self, index):
+        sp = self.__sps[index]
+        idp = self.add_support_vector(sp, sp.y, -self.evaluate(sp.x[sp.y], sp.yv[sp.y]))
+        min_grad_index, min_grad_val = self.min_gradient(index)
+        idn = self.add_support_vector(sp, min_grad_index, min_grad_val)
+        self.SMOstep(idp, idn)
 
     def process_old(self):
-        pass
+        if len(self.__sps) == 0:
+            return
+        index = random.randint(0, len(self.__sps)-1)
+        idp = -1
+        maxGrad = -sys.float_info.max
+        for i in range(len(self.__svs)):
+            if self.__svs[i].x != self.__sps[index]:
+                continue
+            svi = self.__svs[i]
+            if svi.g > maxGrad and svi.b < self.__C*int(svi.y == self.__sps[index].y):
+                idp = i
+                maxGrad = svi.g
+        assert idp != -1
+        if idp == -1:
+            return
+
+        min_grad_index, min_grad_val = self.min_gradient(index)
+        idn = -1
+        for i in range(len(self.__svs)):
+            if self.__svs.x != self.__sps[index]:
+                continue
+            if self.__svs.y == min_grad_index:
+                idn = i
+                break
+        if idn == -1:
+            idn = self.add_support_vector(self.__sps[index], min_grad_index, min_grad_val)
+        self.SMOstep(idp, idn)
 
     def optimize(self):
-        pass
+        if len(self.__sps) == 0:
+            return
+        index = random.randint(0, len(self.__sps)-1)
+        idp = -1
+        idn = -1
+        maxGrad = -sys.float_info.max
+        minGrad = sys.float_info.max
+        for i in range(len(self.__svs)):
+            if self.__svs[i].x != self.__sps[index]:
+                continue
+            svi = self.__svs[i]
+            if svi.g > maxGrad and svi.b < self.__C*int(svi.y == self.__sps[index].y):
+                idp = i
+                maxGrad = svi.g
+            if svi.g < minGrad:
+                idn = i
+                minGrad = svi.g
+        assert idp != -1 and idn != -1
+        if idp == -1 or idn == -1:
+            print "!!!!!!!!!!"
+            return
+        self.SMOstep(idp, idn)
 
-    def add_support_vector(self):
-        pass
+    def add_support_vector(self, x, y, g):
+        sv = Support_vector()
+        sv.b = 0.0
+        sv.x = x
+        sv.y = y
+        sv.g = g
+        index = len(self.__svs)
+        self.__svs.append(sv)
+        x.refCount += 1
 
-    def swap_support_vectors(self):
-        pass
+        for i in range(index):
+            self.__K[i,index] = self.__svs[i].x.x[self.__svs[i].y].dot(x.x[y])
+            self.__K[index,i] = self.__K[i,index]
+        self.__K[index,index] = x.x[y].dot(x.x[y])
+        return index
 
-    def remove_support_vector(self):
-        pass
+    def swap_support_vectors(self, index1, index2):
+        tmp = self.__svs[index1]
+        self.__svs[index1] = self.__svs[index2]
+        self.__svs[index2] = tmp
+
+        row = self.__K[index1]
+        self.__K[index1] = self.__K[index2]
+        self.__K[index2] = row
+
+        col = self.__K[:,index1]
+        self.__K[:,index1] = self.__K[:,index2]
+        self.__K[:,index2] = col
+
+    def remove_support_vector(self, index):
+        self.__svs[index].x.refCount -= 1
+        if self.__svs[index].x.refCount == 0:
+            for i in range(len(self.__sps)):
+                if self.__sps[i] == self.__svs[index].x:
+                    self.__sps.pop(i)
+                    break
+        if index < len(self.__svs)-1:
+            self.swap_support_vectors(index, len(self.__svs)-1)
+            index = len(self.__svs)-1
+        self.__svs.pop()
 
     def budget_maintenance_remove(self):
-        pass
+        minVal = sys.float_info.max
+        idn = -1
+        idp = -1
+        for i in range(len(self.__svs)):
+            if self.__svs[i].b < 0.0:
+                j = -1
+                for k in range(len(self.__svs)):
+                    if self.__svs[k].b >0.0 and self.__svs[k].x == self.__svs[i].x:
+                        j=k
+                        break
+                val = self.__svs[i].b**2*(self.__K[i,i]+self.__K[j,j]-2*self.__K[i,j])
+                if val < minVal:
+                    minVal = val
+                    idn = i
+                    idp = j
+
+        self.__svs[idp].b += self.__svs[idn].b
+        self.remove_support_vector(idn)
+        if idp == len(self.__svs):
+            idp = idn
+        if self.__svs[idp].b < 1e-8:
+            self.remove_support_vector(idp)
+
+        for i in range(len(self.__svs)):
+            svi = self.__svs[i]
+            svi.g = -self.loss(svi.x.yv[svi.y], svi.x.yv[svi.x.y]) - self.evaluate(svi.x.x[svi.y], svi.x.yv[svi.y])
 
     def debug(self):
         print "%s//%s--support patterns/vectors" % (len(self.__sps), len(self.__svs))
--- test_larank.py
+++ test_larank.py
@@ -1,5 +1,22 @@
 import larank
 
 print "world"
-a = larank.svmmodel()
+
+class cc(object):
+    def __init__(self):
+        self.__x = 5
+        self.__y = 3
+        self.z = 4
+    def abc(self):
+        print 'ok'
+
+class made(object):
+    def test(self):
+        ok = cc()
+        print ok.z
+        print 1e-3
+
+c = made()
+c.test()
+a = larank.SVM_model()
 a.test()
